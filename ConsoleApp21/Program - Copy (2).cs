b628b240ebc54ff2f80bf5f41edeaff4af03c245
Davi Ji <v-weji1@microsoft.com>
2021-12-06 06:31:07 +0000

    2

---
+++ ConsoleApp21/Program - Copy (2).cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //456
+            //12312
+        }
+    }
+}
